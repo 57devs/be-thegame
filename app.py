13a1b4bb07a6cf6649884d41c1908379525645c6
Emircan Kucukmotor <47698328+Emircan92@users.noreply.github.com>
2020-05-02 16:13:32 +0300

    dummy data for games socket.

--- app.py
+++ app.py
@@ -168,7 +168,23 @@ async def end_game(request, game_id):
 @app.websocket('/games')
 async def games(request, ws):
 	while True:
-		games = DB().get_lobby_games()
+		# games = DB().get_lobby_games()
+		games = {
+			[
+				{
+					'players': ['ahmet', 'mehmet', 'mahmut'],
+					'game_name': 'mahmutun Oyunu',
+					'created_by': 'Mahmut',
+					'difficulty': 3,
+				},
+				{
+					'players': ['ali', 'veli', 'mehmet'],
+					'game_name': 'alinin oyunu',
+					'created_by': 'ali',
+					'difficulty': 3,
+				},
+			]
+		}
 		await ws.send(json.dumps(games))
 		await asyncio.sleep(1)
 
